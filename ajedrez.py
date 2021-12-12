6aa5ee0f1df2fae835d4b82519af193ba4f9ab6f
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-12 12:26:24 +0100

    creado el tablero inicial correctamente

--- ajedrez.py
+++ ajedrez.py
@@ -1,4 +1,8 @@
 def ajedrez_partida():
     tablero_inicial = "♜\t♞\t♝\t♛\t♚\t♝\t♞\t♜\n♟\t♟\t♟\t♟\t♟\t♟\t♟\t♟\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n♙\t♙\t♙\t♙\t♙\t♙\t♙\t♙\n♖\t♘\t♗\t♕\t♔\t♗\t♘\t♖"
     tablero = []
+    for i in tablero_inicial.split("\n"):
+            tablero.append(i.split("\t"))
+    print(tablero_inicial)
 
+ajedrez_partida()
