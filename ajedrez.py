d33a5042614597697b28adddbcd187cce792c8df
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-12 12:32:56 +0100

    Creado el bucle que te pregunta para hacer más movimientos y que fila y columna quieres mover y a cuál quieres llegar

--- ajedrez.py
+++ ajedrez.py
@@ -4,5 +4,14 @@ def ajedrez_partida():
     for i in tablero_inicial.split("\n"):
             tablero.append(i.split("\t"))
     print(tablero_inicial)
-
+    movimiento =  0
+    while True: #Creo el bucle para preguntar si quiere hacer más movimientos el jugador
+        continuar = input("¿Deseas hacer otro movimiento? si o no ")
+        if continuar != "si":
+            break
+        else:
+            fila_origen = int(input("Introduce la fila de la pieza a mover: "))
+            columna_origen = int(input("Introduce la columna de la pieza a mover: "))
+            fila_destino = int(input("Introduce la fila de destino: "))
+            columna_destino = int(input("Introduce la columna de destino: "))
 ajedrez_partida()
